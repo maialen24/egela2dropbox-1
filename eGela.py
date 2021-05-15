22ff91619db316867e4f660b579af93d5a70317e
JonBarbero <jonbarbero5@gmail.com>
2021-05-15 18:12:03 +0200

    Funtzionalitate berriak :)

--- eGela.py
+++ eGela.py
@@ -5,7 +5,8 @@ import urllib
 import urllib.parse
 import time
 import helper
-
+from urllib.request import Request
+from urllib.request import urlopen
 
 class eGela:
     _login = 0
@@ -99,7 +100,7 @@ class eGela:
         print(metodoa)
         print(location)
         uria = location
-        goiburuak = {'Host': 'egela.ehu.eus',  'Content-Type': 'application/x-www-form-urlencoded',
+        goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
                      'Content-Length': str(len(data)), 'Cookie': self._cookiea}
         data_encoded = urllib.parse.urlencode(data)
         goiburuak['Content-Length'] = str(len(data_encoded))
@@ -136,50 +137,69 @@ class eGela:
         print("\n##### 4. ESKAERA (Ikasgairen eGelako orrialde nagusia) #####")
         metodoa = 'POST'
         data = ""
+        section = 1
+        pdf = []
+        status = 0
         print("Metodoa: ")
         print(metodoa)
-        goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
-                     'Content-Length': str(len(data)), 'Cookie': self._cookiea}
-        uria = "https://egela.ehu.eus/course/view.php?id=42336&section=1"
-        erantzuna = requests.request(metodoa, uria, data=data, headers=goiburuak, allow_redirects=False)
-        edukia = erantzuna.content
+        while status != 404:
+            goiburuak = {'Host': 'egela.ehu.eus', 'Content-Type': 'application/x-www-form-urlencoded',
+                         'Content-Length': str(len(data)), 'Cookie': self._cookiea}
+            uria = "https://egela.ehu.eus/course/view.php?id=42336&section=" + str(section)
+            erantzuna = requests.request(metodoa, uria, data=data, headers=goiburuak, allow_redirects=False)
+            edukia = erantzuna.content
+            status = erantzuna.status_code
+            print(status)
+            if erantzuna.status_code == 200:
+                print("Web Sistemak")
+                soup = BeautifulSoup(erantzuna.content, "html.parser")
+                pdf_results = soup.find_all("div", {"class": "activityinstance"})
+                kop = str(pdf_results).count("pdf")
+                print("PDF kop: " + str(kop))
 
-        if erantzuna.status_code == 200:
-            print("Web Sistemak")
-            soup = BeautifulSoup(erantzuna.content, "html.parser")
-            pdf_results = soup.find_all("div", {"class": "activityinstance"})
-            kop = str(pdf_results).count("pdf")
-            print("PDF kop: " + str(kop))
+            print("\n##### HTML-aren azterketa... #####")
+            soup = BeautifulSoup(edukia, 'html.parser')
+            item_results = soup.find_all('img', {'class': 'iconlarge activityicon'})
+            for each in item_results:
 
-        print("\n##### HTML-aren azterketa... #####")
-        soup = BeautifulSoup(edukia, 'html.parser')
-        item_results = soup.find_all('img', {'class': 'iconlarge activityicon'})
-        for each in item_results:
+                if each['src'].find("/pdf") != -1:
+                    print("\n##### PDF-a bat aurkitu da! #####")
+                    pdf_link = each.parent['href']
+                    uria = pdf_link
+                    if pdf.__contains__(uria):
+                        print("PDF hau " + uria + " aurkituta dago jada!")
+                    else:
+                        headers = {'Host': 'egela.ehu.eus', 'Cookie': self._cookiea}
+                        erantzuna = requests.get(uria, headers=headers, allow_redirects=False)
+                        status = erantzuna.status_code
+                        print(status)
+                        if status == 303:
+                            pdf_uria = erantzuna.headers['Location']
+                            erantzuna = requests.get(pdf_uria, headers=headers, allow_redirects=False)
+                            pdf_link = pdf_uria.split("mod_resource/content/")[1].split("/")[1].replace("%20", "_")
+                            pdf_izena = pdf_link.split('/')[-1]
+                            self._refs.append({'link': pdf_link, 'pdf_name': pdf_izena})
+                        else:
+                            print(metodoa + " " + uria)
+                            kodea = erantzuna.status_code
+                            deskribapena = erantzuna.reason
+                            print(str(kodea) + " " + deskribapena)
+                            edukia = erantzuna.content
+                            status = erantzuna.status_code
+                            soup2 = BeautifulSoup(edukia, 'html.parser')
+                            div_pdf = soup2.find('div', {'class': 'resourceworkaround'})
+                            pdf_link = div_pdf.a['href']
+                            pdf_izena = pdf_link.split('/')[-1]
+                            self._refs.append({'link': pdf_link, 'pdf_name': pdf_izena})
 
-            if each['src'].find("/pdf") != -1:
-                print("\n##### PDF-a bat aurkitu da! #####")
-                pdf_link = each.parent['href']
+                        pdf.append(uria)
 
-                uria = pdf_link
-                headers = {'Host': 'egela.ehu.eus',
-                           'Cookie': self._cookiea}
-                erantzuna = requests.get(uria, headers=headers, allow_redirects=False)
-                print(metodoa + " " + uria)
-                kodea = erantzuna.status_code
-                deskribapena = erantzuna.reason
-                print(str(kodea) + " " + deskribapena)
-                edukia = erantzuna.content
+                progress += 1.5
+                progress_var.set(progress)
+                progress_bar.update()
+                time.sleep(0.1)
 
-                soup2 = BeautifulSoup(edukia, 'html.parser')
-                div_pdf = soup2.find('div', {'class': 'resourceworkaround'})
-                pdf_link = div_pdf.a['href']
-                pdf_izena = pdf_link.split('/')[-1]
-                self._refs.append({'link': pdf_link, 'pdf_name': pdf_izena})
-
-            progress += 1.5
-            progress_var.set(progress)
-            progress_bar.update()
-            time.sleep(0.1)
+            section = section + 1
 
         popup.destroy()
         return self._refs
